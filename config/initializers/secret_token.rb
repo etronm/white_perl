66bf9b94f035ba73fa55eafe82ed742f90232fe3
Enrique Tron <=>
2011-03-03 21:17:31 -0400

    carga inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhitePerl::Application.config.secret_token = '1e21d7b2a2e41fcc260a6259a22eaaec9c76694b83a55acd741131cf67945ba6d8d3c9cb9b34625f17041149abf931ab1ff76073962c79ccd81b65d6439f1a7f'
